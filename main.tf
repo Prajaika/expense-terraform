0b082fe391839b01831fd640f689f5f6ef7bb1d9
Preethi <preethi5014@gmail.com>
2024-04-05 15:29:07 +0800

    changes

--- main.tf
+++ main.tf
@@ -1,33 +1,7 @@
-module "frontend" {
-  depends_on = [module.backend]
-
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "frontend"
-  ssh_user      = var.ssh_user
-  ssh_user      = var.ssh_pass
-  env           = var.env
-  zone_id       = var.zone_id
-}
-
-module "backend" {
-  depends_on = [module.mysql]
-
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "backend"
-  ssh_user      = var.ssh_user
-  ssh_user      = var.ssh_pass
-  env           = var.env
-  zone_id       = var.zone_id
-}
-
 module "mysql" {
   source        = "./modules/app"
   instance_type = var.instance_type
   component     = "mysql"
-  ssh_user      = var.ssh_user
-  ssh_user      = var.ssh_pass
   env           = var.env
   zone_id       = var.zone_id
 }
--- modules/app/variables.tf
+++ modules/app/variables.tf
@@ -1,8 +1,6 @@
 variable "instance_type" {}
 variable "component" {}
 variable "env" {}
-variable "ssh_user" {}
-variable "ssh_pass" {}
 variable "zone_id" {}
 
 
