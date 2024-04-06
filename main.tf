e712dff467f1f580405df3c294439d7989ca15eb
root <root@ip-172-31-1-7.ec2.internal>
2024-04-06 08:07:38 +0000

    Changes from Raghu | Change to vault modules

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "mysql" {
 
 resource "local_file" "foo" {
   #content  = var.vault_token 
-  content  = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+  content  = jsondecode(data.vault_generic_secret.ssh.data_json).pass 
   filename = "/tmp/test"
 }
 
