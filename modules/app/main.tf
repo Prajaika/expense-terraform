5cb3011d82c32f25cb50e79b75d0b6db9558dfe1
Preethi <preethi5014@gmail.com>
2024-04-16 10:38:23 +0800

    Fix changes in Null resource

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -45,6 +45,7 @@ resource "null_resource" "ansible" {
   }
   provisioner "remote-exec" {
     inline = [
+      "rm -f ~/secrets.json ~/app.json",
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/Prajaika/expense-ansible1 get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
       "ansible-pull -i localhost, -U https://github.com/Prajaika/expense-ansible1 expense.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json",
