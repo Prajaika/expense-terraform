39b03b7c86b17c3e7f01c87078605dda4d4a0a9a
Preethi <preethi5014@gmail.com>
2024-04-17 16:28:46 +0800

    Update DNS records

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -49,7 +49,7 @@ resource "null_resource" "ansible" {
       "rm -f ~/secrets.json ~/app.json",
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/Prajaika/expense-ansible1 get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/Prajaika/expense-ansible1 expense.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json",
+      "ansible-pull -i localhost, -U https://github.com/Prajaika/expense-ansible1 expense.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json -e @~/app.json",
     ]
   }
 
